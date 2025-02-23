1915703db48a79127ab68e77f3ab8d631ced4180
Gitesh Sharma <gitesh15796@gmail.com>
2025-02-23 11:06:38 +0530

    data service and user, recipe interface created

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { RecipeInterface } from '../interfaces/recipe-interface';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataService {
+  apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  // getUsers() {
+  //   return this.http.get(this.apiUrl + '/users');
+  // }
+
+  getRecipes(): Observable<RecipeInterface> {
+    return this.http.get<RecipeInterface>(this.apiUrl + '/recipes');
+  }
+}
